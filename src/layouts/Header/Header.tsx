e6a6005ae93e6308eb62aa2673cef24687842887
HiagoBDev <hiagoborgaco@hotmail.com>
2024-06-07 14:54:35 -0300

    Header component Done

---
+++ src/layouts/Header/Header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header className=" w-full pt-20 flex items-center justify-center">
+      <h1 className=" text-5xl text-[#4ec7ff]">To Do List</h1>
+    </header>
+  )
+}
